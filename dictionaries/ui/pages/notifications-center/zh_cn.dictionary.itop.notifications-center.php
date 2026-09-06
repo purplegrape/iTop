a687157786c754864e2d6f68276dd50ba871aa54
purple grape <purplegrape4@gmail.com>
2026-09-07 00:03:27 +0800

    Update zh_cn.dictionary.itop.notifications-center.php

--- dictionaries/ui/pages/notifications-center/zh_cn.dictionary.itop.notifications-center.php
+++ dictionaries/ui/pages/notifications-center/zh_cn.dictionary.itop.notifications-center.php
@@ -14,7 +14,7 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'UI:NotificationsCenter:Page:Title' => '通知中心',
 	'UI:NotificationsCenter:Panel:Title' => '通知中心',
 	'UI:NotificationsCenter:Panel:SubTitle' => '管理收到的通知 : 取消订阅或限制到单一频道',
-	'UI:NotificationsCenter:Panel:Toolbar:ViewAllNews:Title' => '查看所有新闻',
+	'UI:NotificationsCenter:Panel:Toolbar:ViewAllNews:Title' => '查看所有消息',
 	'UI:NotificationsCenter:Panel:Table:Channels' => '频道',
 	'UI:NotificationsCenter:Unsubscribe:Success' => '您已成功取消订阅所选通知.',
 	'UI:NotificationsCenter:Unsubscribe:Error' => '取消订阅所选通知时发生错误.',
