22c42d3c79cca8c20c6c45f3b9c0669593e54271
purple grape <purplegrape4@gmail.com>
2026-09-10 23:43:44 +0800

    Update zh_cn.dict.itop-bridge-cmdb-services.php

--- datamodels/2.x/itop-bridge-cmdb-services/dictionaries/zh_cn.dict.itop-bridge-cmdb-services.php
+++ datamodels/2.x/itop-bridge-cmdb-services/dictionaries/zh_cn.dict.itop-bridge-cmdb-services.php
@@ -38,13 +38,13 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'Class:lnkFunctionalCIToProviderContract+' => 'This link models the functional CIs that are supported by an external company through a provider contract.~~',
 	'Class:lnkFunctionalCIToProviderContract/Name' => '%1$s / %2$s',
 	'Class:lnkFunctionalCIToProviderContract/Attribute:providercontract_id' => '供应商合同',
-	'Class:lnkFunctionalCIToProviderContract/Attribute:providercontract_id+' => '~~',
+	'Class:lnkFunctionalCIToProviderContract/Attribute:providercontract_id+' => '',
 	'Class:lnkFunctionalCIToProviderContract/Attribute:providercontract_name' => '供应商合同名称',
-	'Class:lnkFunctionalCIToProviderContract/Attribute:providercontract_name+' => '~~',
+	'Class:lnkFunctionalCIToProviderContract/Attribute:providercontract_name+' => '',
 	'Class:lnkFunctionalCIToProviderContract/Attribute:functionalci_id' => '配置项',
-	'Class:lnkFunctionalCIToProviderContract/Attribute:functionalci_id+' => '~~',
+	'Class:lnkFunctionalCIToProviderContract/Attribute:functionalci_id+' => '',
 	'Class:lnkFunctionalCIToProviderContract/Attribute:functionalci_name' => '配置项名称',
-	'Class:lnkFunctionalCIToProviderContract/Attribute:functionalci_name+' => '~~',
+	'Class:lnkFunctionalCIToProviderContract/Attribute:functionalci_name+' => '',
 ]);
 
 //
@@ -56,13 +56,13 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'Class:lnkFunctionalCIToService+' => 'This link models the functional CIs that are required to deliver a Service and whose malfunction would affect the quality of the Service.~~',
 	'Class:lnkFunctionalCIToService/Name' => '%1$s / %2$s',
 	'Class:lnkFunctionalCIToService/Attribute:service_id' => '服务',
-	'Class:lnkFunctionalCIToService/Attribute:service_id+' => '~~',
+	'Class:lnkFunctionalCIToService/Attribute:service_id+' => '',
 	'Class:lnkFunctionalCIToService/Attribute:service_name' => '服务名称',
-	'Class:lnkFunctionalCIToService/Attribute:service_name+' => '~~',
+	'Class:lnkFunctionalCIToService/Attribute:service_name+' => '',
 	'Class:lnkFunctionalCIToService/Attribute:functionalci_id' => '配置项',
-	'Class:lnkFunctionalCIToService/Attribute:functionalci_id+' => '~~',
+	'Class:lnkFunctionalCIToService/Attribute:functionalci_id+' => '',
 	'Class:lnkFunctionalCIToService/Attribute:functionalci_name' => '配置项名称',
-	'Class:lnkFunctionalCIToService/Attribute:functionalci_name+' => '~~',
+	'Class:lnkFunctionalCIToService/Attribute:functionalci_name+' => '',
 ]);
 
 //
