7000186d48dd95ab0076750463bdd9c74cfa66ff
purple grape <purplegrape4@gmail.com>
2026-09-14 23:17:52 +0800

    Update zh_cn.dict

--- datamodels/2.x/itop-portal/dictionaries/zh_cn.dict.itop-portal.php
+++ datamodels/2.x/itop-portal/dictionaries/zh_cn.dict.itop-portal.php
@@ -1,22 +1,41 @@
 <?php
 
+// Copyright (C) 2010-2024 Combodo SAS
+//
+//   This file is part of iTop.
+//
+//   iTop is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   iTop is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with iTop. If not, see <http://www.gnu.org/licenses/>
 /**
- * Localized data
- *
- * @copyright Copyright (C) 2010-2024 Combodo SAS
- * @license    https://opensource.org/licenses/AGPL-3.0
- *
+ * @author      Benjamin Planque <benjamin.planque@combodo.com>
+ * @copyright   Copyright (C) 2010-2024 Combodo SAS
+ * @license     https://opensource.org/licenses/AGPL-3.0
  */
-/**
- * @author Benjamin Planque <benjamin.planque@combodo.com>
- *
- */
+
+//////////////////////////////////////////////////////////////////////
+// Note: The classes have been grouped by categories: bizmodel
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+// Classes in 'bizmodel'
+//////////////////////////////////////////////////////////////////////
+//
+
 Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'portal:itop-portal' => '标准门户',
 	'Page:DefaultTitle' => '%1$s - 用户门户',
 	'Brick:Portal:UserProfile:Title' => '我的设置',
 	'Brick:Portal:NewRequest:Title' => '新建工单',
-	'Brick:Portal:NewRequest:Title+' => '<p>需要帮助?</p><p>选择子服务, 然后提交工单给我们的支持团队.</p>',
+	'Brick:Portal:NewRequest:Title+' => '<p>需要帮助?</p><p>选择适当的服务目录, 然后提交工单给我们的支持团队.</p>',
 	'Brick:Portal:OngoingRequests:Title' => '正在处理的工单',
 	'Brick:Portal:OngoingRequests:Title+' => '<p>跟踪正在处理的工单.</p><p>查询进度, 添加评论, 添加附件, 确认解决方案.</p>',
 	'Brick:Portal:OngoingRequests:Tab:OnGoing' => '正在处理',
