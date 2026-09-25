3dacb75ef6edcd418b3f17a14d5cf16c2d5d1592
purple grape <purplegrape4@gmail.com>
2026-09-25 09:58:11 +0800

    Update zh_cn.dictionary.itop.ui.php

--- datamodels/2.x/itop-attachments/dictionaries/zh_cn.dict.itop-attachments.php
+++ datamodels/2.x/itop-attachments/dictionaries/zh_cn.dict.itop-attachments.php
@@ -96,8 +96,8 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'Class:TriggerOnAttachmentDelete'                          => '触发器 (对象附件被删除时)',
 	'Class:TriggerOnAttachmentDelete+'                         => '触发器 (对象附件被删除时)',
 	'Class:TriggerOnAttachmentCreate/Attribute:file_in_email'  => '在邮件里添加文件',
-	'Class:TriggerOnAttachmentCreate/Attribute:file_in_email+' => '如果勾选，文件将在触发邮件操作时自动附加到邮件中',
+	'Class:TriggerOnAttachmentCreate/Attribute:file_in_email+' => '如果勾选，文件将会在触发邮件操作时自动附加到邮件中',
 	'Class:TriggerOnAttachmentDelete/Attribute:file_in_email'  => '在邮件里添加已删除的文件',
-	'Class:TriggerOnAttachmentDelete/Attribute:file_in_email+' => '如果勾选，已删除的文件将在触发邮件操作时自动附加到邮件中',
+	'Class:TriggerOnAttachmentDelete/Attribute:file_in_email+' => '如果勾选，已删除的文件将会在触发邮件操作时自动附加到邮件中',
 	'Class:TriggerOnObject:TriggerClassAttachment/ReadOnlyMessage' => '此触发器不允许用于附件. 请使用特定的触发器',
 ]);
