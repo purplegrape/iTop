4211a74d1ed8aabf83d766c325950e12047326bc
purple grape <purplegrape4@gmail.com>
2026-09-25 19:12:55 +0800

    Update zh_cn.dict.itop-portal-base.php

--- datamodels/2.x/itop-portal-base/dictionaries/zh_cn.dict.itop-portal-base.php
+++ datamodels/2.x/itop-portal-base/dictionaries/zh_cn.dict.itop-portal-base.php
@@ -84,7 +84,7 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'Brick:Portal:UserProfile:Password:ConfirmPassword' => '确认密码',
 	'Brick:Portal:UserProfile:Password:CantChangeContactAdministrator' => '要修改密码, 请联系管理员',
 	'Brick:Portal:UserProfile:Password:CantChangeForUnknownReason' => '无法修改密码, 请联系管理员',
-	'Brick:Portal:UserProfile:PersonalInformations:Title' => '个体信息',
+	'Brick:Portal:UserProfile:PersonalInformations:Title' => '个人信息',
 	'Brick:Portal:UserProfile:Photo:Title' => '头像',
 ]);
 
